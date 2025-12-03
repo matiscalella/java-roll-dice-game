ce1ec0b0af297382e5aa34aae6193233f161bcc8
Matias Scalella <matias.scalella@gmail.com>
2025-12-03 20:42:51 -0300

    Refactor dice logic into Dice class and decouple from Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,8 @@
-
 package main;
 
-import java.util.Random;
 import java.util.Scanner;
+import main.Player;
+
 /**
  * Main class for the Dice Roll Game.
  * <p>
@@ -26,7 +26,7 @@ public class Main {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        Dice dice = new Dice();
         int numberOfDice;
         int total = 0;
         
@@ -40,15 +40,15 @@ public class Main {
         
         Player player1 = new Player(player1Name);
         Player player2 = new Player(player2Name);
-        
-        System.out.println("--- Turn: ["+ player1.getName() + "] ---");
         System.out.print("Enter the number of dice to roll: ");
         numberOfDice = scanner.nextInt();
-        
+
+        System.out.println("--- Turn: ["+ player1.getName() + "] ---");
+     
         if (numberOfDice > 0) {
             System.out.println("\nRolling the dice for "+ player1.getName() +"...\n");
             for (int i = 0; i < numberOfDice; i++) {
-                int roll = animateRoll(random);
+                int roll = dice.roll();
                 System.out.println(player1.getName() +" rolled: [" + roll + "]");
                 player1.addToScore(roll);
             }
@@ -57,14 +57,12 @@ public class Main {
             System.out.println("Please enter a number of dice greater than 0 (zero).");
         }
         
-        System.out.println("--- Turn: ["+ player2.getName() + "] ---");
-        System.out.print("Enter the number of dice to roll: ");
-        numberOfDice = scanner.nextInt();
+        System.out.println("\n--- Turn: ["+ player2.getName() + "] ---");
         
         if (numberOfDice > 0) {
             System.out.println("\nRolling the dice for "+ player2.getName() +"...\n");
             for (int i = 0; i < numberOfDice; i++) {
-                int roll = animateRoll(random);
+                int roll = dice.roll();
                 System.out.println(player2.getName() +" rolled: [" + roll + "]");
                 player2.addToScore(roll);
             }
@@ -86,108 +84,8 @@ public class Main {
         else {
             System.out.println("It's a tie!");
         }
-        
-        
-        
-        
+
         scanner.close();
-    }
-    /**
-     * Prints the ASCII representation of a die based on the rolled value.
-     * <p>
-     * Each die face is represented using simple ASCII characters to ensure
-     * compatibility with any terminal.
-     * </p>
-     *
-     * @param roll the value of the die (from 1 to 6)
-     */
-    static void printDie(int roll) {
-        String dice1 = """
-        -------
-        |     |
-        |  o  |
-        |     |
-        -------
-        """;
-
-        String dice2 = """
-        -------
-        | o   |
-        |     |
-        |   o |
-        -------
-        """;
-
-        String dice3 = """
-        -------
-        | o   |
-        |  o  |
-        |   o |
-        -------
-        """;
-
-        String dice4 = """
-        -------
-        | o o |
-        |     |
-        | o o |
-        -------
-        """;
-
-        String dice5 = """
-        -------
-        | o o |
-        |  o  |
-        | o o |
-        -------
-        """;
-
-        String dice6 = """
-        -------
-        | o o |
-        | o o |
-        | o o |
-        -------
-        """;
-        
-        switch (roll) {
-            case 1 -> System.out.print(dice1);
-            case 2 -> System.out.print(dice2);
-            case 3 -> System.out.print(dice3);
-            case 4 -> System.out.print(dice4);
-            case 5 -> System.out.print(dice5);
-            case 6 -> System.out.print(dice6);
-            default -> System.out.println("Invalid roll.");
-        }
-    }
-    /**
-     * Simulates a dice roll animation by printing several intermediate
-     * random dice faces before returning the final rolled value.
-     *
-     * @param random the Random instance used to generate values
-     * @return the final rolled value (1 to 6)
-     */
-    static int animateRoll(Random random) {
-        int animationLength = 5;
-        int lastRoll = 0;
-        for (int i = 0; i < 5; i++) {
-            if (i == animationLength - 1) {
-                lastRoll = random.nextInt(1, 7);
-                printDie(lastRoll);
-                break;
-            } else {
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
-                lastRoll = random.nextInt(1, 7);
-                printDie(lastRoll);
-                try {
-                    Thread.sleep(180);
-                } catch (InterruptedException e) {
-                    System.out.println("Error");
-                }
-            }
-        }
-        return lastRoll;
     }
     
 }
