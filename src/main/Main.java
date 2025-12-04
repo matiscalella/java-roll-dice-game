1bc9ea3e584cc7aa11d7cd1b94b70c93be8c24d6
Matias Scalella <matias.scalella@gmail.com>
2025-12-03 21:04:29 -0300

    Refactor game flow into Game class with validation and clean Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,14 @@
 package main;
 
 import java.util.Scanner;
-import main.Player;
+
 
 /**
  * Main class for the Dice Roll Game.
  * <p>
- * This console-based application allows the user to roll one or more dice,
- * displays each die using ASCII art, and calculates the total score.
+ * This console-based application initializes the players, gathers user input,
+ * and starts the game session.
  * </p>
- *
- * <p>
- * The game demonstrates basic Java concepts such as:
- * </p>
- * <ul>
- *   <li>User input with Scanner</li>
- *   <li>Random number generation</li>
- *   <li>Loops and conditional logic</li>
- *   <li>Methods and modular design</li>
- * </ul>
  *
  * @author Matias
  */
@@ -28,7 +18,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Dice dice = new Dice();
         int numberOfDice;
-        int total = 0;
         
         System.out.println("----- DICE ROLLER GAME -----");
         
@@ -43,47 +32,8 @@ public class Main {
         System.out.print("Enter the number of dice to roll: ");
         numberOfDice = scanner.nextInt();
 
-        System.out.println("--- Turn: ["+ player1.getName() + "] ---");
-     
-        if (numberOfDice > 0) {
-            System.out.println("\nRolling the dice for "+ player1.getName() +"...\n");
-            for (int i = 0; i < numberOfDice; i++) {
-                int roll = dice.roll();
-                System.out.println(player1.getName() +" rolled: [" + roll + "]");
-                player1.addToScore(roll);
-            }
-            System.out.println("\nTotal score for " + player1.getName() + ": " + player1.getScore());
-        } else {
-            System.out.println("Please enter a number of dice greater than 0 (zero).");
-        }
-        
-        System.out.println("\n--- Turn: ["+ player2.getName() + "] ---");
-        
-        if (numberOfDice > 0) {
-            System.out.println("\nRolling the dice for "+ player2.getName() +"...\n");
-            for (int i = 0; i < numberOfDice; i++) {
-                int roll = dice.roll();
-                System.out.println(player2.getName() +" rolled: [" + roll + "]");
-                player2.addToScore(roll);
-            }
-            System.out.println("\nTotal score for " + player2.getName() + ": " + player2.getScore());
-        } else {
-            System.out.println("Please enter a number of dice greater than 0 (zero).");
-        }
-        
-        System.out.println("\n--- END OF TURNS ---");
-        System.out.println("FINAL SCORES: ");
-        System.out.println(player1.getName() + ": [" + player1.getScore() + "]");
-        System.out.println(player2.getName() + ": [" + player2.getScore() + "]");
-        if (player1.getScore() > player2.getScore()) {
-            System.out.println("\n🏆 Winner: " + player1.getName());
-        } 
-        else if (player2.getScore() > player1.getScore()) {
-            System.out.println("\n🏆 Winner: " + player2.getName());
-        } 
-        else {
-            System.out.println("It's a tie!");
-        }
+        Game game = new Game(player1, player2, dice, numberOfDice);
+        game.play();
 
         scanner.close();
     }
